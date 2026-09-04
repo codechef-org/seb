8dde4b000c8b92516cb9947d2accbe92340a435a
Pushpender Singh <singhpushpender250@gmail.com>
2026-09-04 17:37:43 +0530

    Windows bat script

--- seb.bat
+++ seb.bat
@@ -1,9 +1,11 @@
-@echo off 
+@echo off
 setlocal
 
+set "CONTEST_CODE=SEB"
 set "SCRIPT_URL=https://seb.cchef.co/seb.ps1"
 
-powershell.exe -NoProfile -ExecutionPolicy Bypass - Command ^
-   "& ([scriptblock]:: Create( (Invoke-RestMethod '%SCRIPT_URL%'))) %*"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ^
+  "& ([scriptblock]::Create((Invoke-RestMethod '%SCRIPT_URL%'))) -ContestCode '%CONTEST_CODE%'"
 
 endlocal
+pause
